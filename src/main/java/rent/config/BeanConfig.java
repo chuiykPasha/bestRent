f8d8819eb1756c492a97f2a84e040604584e2e31
chuiykPasha <p4y10k@gmail.com>
2019-05-02 11:41:49 +0300

    enable transaction manager

--- src/main/java/rent/config/BeanConfig.java
+++ src/main/java/rent/config/BeanConfig.java
@@ -24,6 +24,7 @@ import java.util.Locale;
 
 @Configuration
 @EnableJpaRepositories(value = "rent.repository")
+@EnableTransactionManagement
 public class BeanConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
