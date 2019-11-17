bbc0b54573f6eae16ea0d7b8f74275caf4b2226b
chuiykPasha <p4y10k@gmail.com>
2019-11-17 14:07:27 +0200

    fix wrong query

--- src/main/java/rent/repository/UserRepository.java
+++ src/main/java/rent/repository/UserRepository.java
@@ -2,11 +2,12 @@ package rent.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import rent.entities.User;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-    @Query("Select u FROM User u join u.roles r")
-    User findByEmail(String email);
+    @Query("Select u FROM User u join u.roles r where u.email = :email")
+    User findByEmail(@Param("email")String email);
 }
